feb20e94e42d25dcfb1d8ce6a7b72900f25c34ae
yourname <shuanghuinan@163.com>
2020-03-15 19:43:40 +0800

    完成layout组件的基本构造

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>公用布局</div>
+  <div class="container">
+    <!-- 导航栏 -->
+    <van-nav-bar fixed title="黑马头条" right-text="搜索"  @click-right="$router.push('/search')" v-if="$route.path!=='/user'"></van-nav-bar>
+    <!-- 二级路由容器 -->
+    <div class="my-wrapper" :class="{noTop:$route.path=='/user'}">
+        <router-view></router-view>
+    </div>
+    <!-- 标签栏 -->
+    <van-tabbar route>
+       <van-tabbar-item icon="home-o" to='/'>首页</van-tabbar-item>
+       <van-tabbar-item icon="chat-o" to='/question'>问答</van-tabbar-item>
+       <van-tabbar-item icon="video-o" to='/video'>视频</van-tabbar-item>
+       <van-tabbar-item icon="user-circle-o" to='/user'>我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  // 控制二级路由容器的高宽
+  .my-wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop {
+      padding-top: 0;
+    }
+  }
+}
 </style>
