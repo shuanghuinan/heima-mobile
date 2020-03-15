1a3a6a37aa8fb6d0ba9627493acc1cd4ca539103
yourname <shuanghuinan@163.com>
2020-03-15 13:47:43 +0800

    vant组件库的引入及全局注册

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant' // 引入vant组件库
+import 'vant/lib/index.css'// 引入vant样式文件
+Vue.use(Vant)// 全局注册vant
 Vue.config.productionTip = false
 
 new Vue({
