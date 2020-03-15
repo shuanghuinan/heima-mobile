45aa2ad97863312a182fbb333e79defbaf377a1f
yourname <shuanghuinan@163.com>
2020-03-15 17:35:41 +0800

    rem适配第二步---amfe-flexible插件的安装及引入

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入vant组件库
 import 'vant/lib/index.css'// 引入vant样式文件
+import 'amfe-flexible'// 引入rem适配的插件---会根据当前屏幕变化而产生不同的font-size
 Vue.use(Vant)// 全局注册vant
 Vue.config.productionTip = false
 
