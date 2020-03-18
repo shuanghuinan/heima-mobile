31abb9eb01fc67a680e88a901e142937e494acfa
yourname <shuanghuinan@163.com>
2020-03-18 10:29:47 +0800

    修改vant组件库的主题色

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant' // 引入vant组件库
-import 'vant/lib/index.css'// 引入vant样式文件
+import 'vant/lib/index.less'// 引入vant样式文件
 import 'amfe-flexible'// 引入rem适配的插件---会根据当前屏幕变化而产生不同的font-size
 import '@/styles/index.less'// 引入公共样式
 import '@/permission' // 引入导航守卫
