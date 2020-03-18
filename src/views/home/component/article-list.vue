46f6eeac0d567b5d40e02fcaa36ab8578a4bbb7f
yourname <shuanghuinan@163.com>
2020-03-18 19:53:25 +0800

    封装article-list组件

---
+++ src/views/home/component/article-list.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 我是每个tab -->
+  <div class="scroll-wrapper">
+      <van-list v-model="loading" :finished="finished" @load="load"></van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false, // 表示当前是否在上拉加载
+      finished: false // 表示当前列表的所有数据是否已经加载完毕
+    }
+  },
+  methods: {
+    // 此方法在滚动条与底部举例小于offset时触发
+    load () {
+    //   this.loading = true
+      this.finished = true
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>首页</div>
+  <div class="container">
+    <!-- tabs标签页组件 -->
+    <van-tabs>
+      <!-- 标签页内的每一项 -->
+      <van-tab :title="`标签${item}`" v-for="item in 10" v-bind:key="item">
+        <!-- 每项的内容,单独封装成一个组件 -->
+        <articleList>
+
+        </articleList>
+      </van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
+import articleList from '@/views/home/component/article-list'
 export default {
 
+  components: {
+    articleList
+  }
 }
 </script>
 
